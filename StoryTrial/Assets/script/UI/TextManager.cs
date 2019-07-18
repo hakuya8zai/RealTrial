520e76344c12a533d20e92931bf2b4d2d0856366
Unknown <hakuya8zai@gmail.com>
2019-07-18 09:23:00 +0800

    StoryOne

---
+++ StoryTrial/Assets/script/UI/TextManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextManager : MonoBehaviour {
+    public static GameObject TextPoint;
+
+    public static string[] StoryOne;
+
+
+
+
+	// Use this for initialization
+	void Start () {
+        TextPoint = GameObject.Find("01");
+        StoryOne[0] = ("出發！郊遊囉！總之先以那個亮亮的地方當作目的地吧！");
+        StoryOne[1] = ("大姊，請您認真一點，我們現在可是肩負著國家的命運，要是失敗了……");
+        StoryOne[2] = ("你的身體這麼僵硬可不行！要和我多學著點！你看，多麼光滑柔軟！");
+        StoryOne[3] = ("我們兩個明明看起來就是一樣的。");
+        StoryOne[4] = ("大姊，請加快速度吧，不然再這樣下去真的會追不上他們的。");
+        StoryOne[5] = ("看來小弟你也很想去露營嘛！很好很好！衝阿！");
+        StoryOne[6] = ("露營……？");
+        StoryOne[7] = ("不好意思，剛剛不小心扭到腳了……沒關係，繼續前進吧！");
+        StoryOne[8] = ("……唉呀！不小心踩到陷阱了呢！只好放慢速度了。");
+        StoryOne[9] = ("大姊，謝謝妳，我已經沒問題了，請回到正常速度吧！");
+        StoryOne[10] = ("可是我還沒休息夠……");
+        StoryOne[11] = ("……請加快速度。");
+        StoryOne[12] = ("");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
---
+++ StoryTrial/Assets/script/UI/UIFollow.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class UIFollow : MonoBehaviour {
+
+    
+    private Camera cam;
+    public static Text theySay;
+    private float offsetX = 600.0f;
+    private float offsetY = -150.0f;
+    private float smoothTime = 1.0f;
+    private Vector3 cameraVelocity = Vector3.zero;
+
+
+    // Use this for initialization
+    private void Awake()
+    {
+        theySay = GetComponent<Text>();
+    }
+    void Start () {
+        cam = Camera.main;
+        Vector3 screenPos = cam.WorldToScreenPoint(CameraFollow.target.position) + new Vector3(offsetX, offsetY, 0);
+        theySay.rectTransform.position = screenPos;
+}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log(theySay.text);
+        Vector3 screenPos = cam.WorldToScreenPoint(CameraFollow.target.position) + new Vector3(offsetX, offsetY, 0);
+        theySay.rectTransform.position = Vector3.SmoothDamp(theySay.rectTransform.position,screenPos,ref cameraVelocity,smoothTime);
+	}
+
+    public static void TextFadeIn()
+    {
+
+        theySay.CrossFadeAlpha(225, 0.0f, false);
+
+    }
+    public static void TextFadeOut()
+    {
+        theySay.CrossFadeAlpha(0, 0.0f, false);
+    }
+}
